1b8e234d767f5aa270552eca967bc00cb1d25332
Sergei <s.belinski.vironit@vironit.ru>
2019-04-26 21:47:56 +0300

    fix: fix bugs

--- services/userAccountService.js
+++ services/userAccountService.js
@@ -1,24 +1,18 @@
 const UserModel = require('../models/user');
-const passport = require('passport')
 
 class UserAccountService {
     getUserInfo(req, res, next){
-        console.log(req.params._id + ' tuta');
-            UserModel.findOne({_id: req.params._id}).then(data=>{
-                console.log(data)
-                res.json(data);
-            });
+        UserModel.findOne({_id: req.params._id}).then(data=>{
+            res.json(data);
+        });
     }
 
 
     updateUserInfo(req, res, next){
-        console.log(req.body.login);
-        UserModel.findOneAndUpdate({login: req.body.login}, req.body).then( (data) =>{
-                console.log(data)
-        }
+        UserModel.findOneAndUpdate({login: req.body.login}, req.body).then( (data) => res.sendStatus(200)
         );
-        res.send('asdf')
     }
+
 }
 
 let userAccountService = new UserAccountService();
