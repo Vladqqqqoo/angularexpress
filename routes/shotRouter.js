41813029f31f02b2212cdb75eee43e219b2b2f62
Vladislav <v.drobovich.vironit@vironit.ru>
2019-05-11 11:06:27 +0300

    fix: fix auth with shots list

--- routes/shotRouter.js
+++ routes/shotRouter.js
@@ -3,14 +3,16 @@ const router = express.Router();
 const shotController = require('../controllers/shotController');
 const passport = require('passport');
 
-router.use(passport.authenticate('jwt', {session: false}));
+// router.use(passport.authenticate('jwt', {session: false}));
 
-router.get('/user/list', shotController.getUserList);
+router.get('/user/list', passport.authenticate('jwt', {session: false}), shotController.getUserList);
+//Только свои посты
+
 router.get('/list', shotController.getList);
-router.post('/', shotController.uploadOneShot);
+router.post('/', passport.authenticate('jwt', {session: false}), shotController.uploadOneShot);
 
 
 router.get('/:id', shotController.getShot);
-router.put('/:id', shotController.updateShot);
+router.put('/:id',passport.authenticate('jwt', {session: false}), shotController.updateShot);
 
 module.exports = router;
