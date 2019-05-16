ba588d1c4456ef3abf5494a15021e78f775a54cd
Sergei <s.belinski.vironit@vironit.ru>
2019-05-16 22:00:36 +0300

    feat: add shot aggregating with user

--- services/commentService.js
+++ services/commentService.js
@@ -1,28 +1,67 @@
 const commentModel = require('../models/comment');
+const mongoose = require("mongoose");
 
 class CommentService {
-    createComment(req, res, next){
-        commentModel.create(req.body)
-            .then(
-                comment => {
-                    res.send(comment)
-                }
-            )
-            .catch(
-                err => console.log(err)
-            )
+    async createComment(req, res, next){
+        await commentModel.create(req.body).then(
+            data => {
+                commentModel.aggregate([
+                    {
+                        $match: {
+                            "shotId": {$eq: mongoose.Types.ObjectId(req.body.shotId)},
+                            "_id": {$eq: mongoose.Types.ObjectId(data._id)}
+                        }
+                    },
+                    {
+                        $lookup: {
+                            localField: 'commentatorId',
+                            from: 'users',
+                            foreignField: '_id',
+                            as: 'user'
+                        }
+                    },
+                    {
+                        $unwind: {
+                            'path': '$user',
+                            'preserveNullAndEmptyArrays': true
+                        }
+                    }
+                ])
+                    .then(
+                        comment => {
+                            console.log(comment);
+                            res.send(comment)
+                        }
+                    );
+
+            }
+        )
+
     }
 
-    getAllComments(req, res, next){
-        commentModel.find({shotId: req.params.id})
-            .then(
-                allComments => {
-                    res.send(allComments);
-                    }
-            )
-            .catch(
-                err => console.log(err)
-            )
+    async getAllComments(req, res, next){
+        const comment = await commentModel.aggregate([
+            {
+                $match: {
+                    "shotId": {$eq : mongoose.Types.ObjectId(req.params.id)},
+                }
+            },
+            {
+                $lookup: {
+                    localField: 'commentatorId',
+                    from: 'users',
+                    foreignField: '_id',
+                    as: 'user'
+                }
+            },
+            {
+                $unwind: {
+                    'path': '$user',
+                    'preserveNullAndEmptyArrays': true
+                }
+            }
+        ]);
+        res.send(comment)
     }
 }
 
